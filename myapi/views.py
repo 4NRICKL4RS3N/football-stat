b78a3a159a75d559c1f405a8074b360bad74cdc5
L4RS3N <94544098+4NRICKL4RS3N@users.noreply.github.com>
2023-12-04 13:55:27 +0300

    Update views.py

--- myapi/views.py
+++ myapi/views.py
@@ -7,7 +7,7 @@ from .serializers import GeneralStatSerializer
 class StatView(APIView):
     def get(self, request):
         with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM v_general_general;")
+            cursor.execute("SELECT * FROM v_defense_general;")
             rows = cursor.fetchall()
         
         data = [{'equipe': row[0], 'competition': row[1], 'buts': row[2], 'tirs_pm': row[3], 'carton_j': row[4], 'carton_r': row[5], 'possession': row[6], 'passes_reussies': row[7], 'aeriens_gagnes': row[8], 'note': row[9]} for row in rows]
