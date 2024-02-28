6019ef5d61c306c1d2f9f0dd3c0931c255b13346
4NRICKL4RS3N <94544098+4NRICKL4RS3N@users.noreply.github.com>
2024-02-28 16:54:36 +0300

    Update urls.py

--- footballStat/urls.py
+++ footballStat/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from myapi import index
 
 urlpatterns = [
     path('myapi/', include('myapi.urls')),
+    path('index/', index, name="index"),
     path('admin/', admin.site.urls),
 ]
